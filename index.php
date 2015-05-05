16b76feeabf56173dba0f27083ba66563d161b30
mdymen <msn@dymenstein.com>
2015-05-05 08:39:29 -0300

    index

--- index.php
+++ index.php
@@ -125,7 +125,7 @@
                  <div class="col-xs-12 col-md-3">
                                             <div class="widget flat radius-bordered">
                                                 <div class="">
-                                                    <span class=""><h2>Cadástre-se</h2></span>
+                                                    <span class=""><h2>Cadastre-se</h2></span>
                                                 </div>
                                                 <div class="">
                                                     <div id="registration-form">
@@ -670,5 +670,16 @@
           conf();
         });
     });
+
+</script>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-62612806-1', 'auto');
+  ga('send', 'pageview');
 
 </script>
